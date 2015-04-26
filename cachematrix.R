f7c311d7c56b200edab3f85472378928e9c1996f
haverm <haverm@users.noreply.github.com>
2015-04-26 13:33:45 +0200

    First complete version
    Maybe it does not work.

--- cachematrix.R
+++ cachematrix.R
@@ -4,15 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
+        m <- NULL                       ##We will denote m as the inverse of the matrix
+        set <- function(y) {            ##The same function in the code provided by the course. Set x to the value of y
+                x <<- y                 ## & set the inverse to NULL
                 m <<- NULL
         }
-        get <- function() x
-        setinverse <- function(inverse) m <<- inverse
-        getinverse <- function() m
-        list(set = set, get = get,
+        get <- function() x             ##Get the original matrix X
+        setinverse <- function(inverse) m <<- inverse ##Set the inverse of X
+        getinverse <- function() m      ##Get the inverse of X
+        list(set = set, get = get,      ##Group all functions in a vector
              setinverse = setinverse,
              getinverse = getinverse)
 }
@@ -21,5 +21,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse() ##Get the inverse from the makeCacheMatrix function
+        if(!is.null(m)) {   ##If is different from NULL
+                message("getting cached data")
+                return(m)   ##Return the cached data
+        }
+        data <- x$get()  ##Else, get the original matrix X
+        m <- solve(data, ...) ##Solve function over data will do the inverse of x and save it in m
+        x$setinverse(m)  ##This line saves the value of m in the makeCacheMatrix function.
+        m       ## Return a matrix that is the inverse of 'x'
 }
